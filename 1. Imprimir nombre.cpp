6810119a0b3ab6d26eb5b11fde26e810c5b436d5
YarethziGiselle <30918399+YarethziGiselle@users.noreply.github.com>
2017-08-14 09:30:46 -0500

    Add files via upload

---
+++ 1. Imprimir nombre.cpp
@@ -0,0 +1,16 @@
+/*31-1-17
+Programa que utiliza fn printf()*/
+#include <stdio.h>
+int main( )
+{
+      /*Imprimir caracteres comunes para formar un nombre de persona*/
+      printf("\n\n\n\n\t\t\t%s", "Yarethzi\n\n");
+      /*Imprimir valor un valor entero 10*/
+      printf("\t\t\tValor entero: %d\n\n",10);
+      /*Imprimir la suma de 4+2*/
+      printf("\t\t\tLa suma de: %d + %d = %d\n\n", 4, 2, 4+2);
+      /*Imprimir el valor de una variable*/
+      float num = 4.9;
+      printf("\t\t\tEl valor de la variable num es: %.2f\n\n\n\n",num);
+}
+/*Fin de main( )*/
