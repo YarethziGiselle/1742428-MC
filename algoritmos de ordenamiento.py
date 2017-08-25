10d34650bfc415d20a730046a727149574211a7b
YarethziGiselle <30918399+YarethziGiselle@users.noreply.github.com>
2017-08-24 20:08:52 -0500

    Add files via upload

---
+++ algoritmos de ordenamiento.py
@@ -0,0 +1,59 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import random
+>>> p=[]
+>>> for i in range(100):
+	p.append(i)
+
+	
+>>> def minimo(yar):
+	aux=yare #arreglo auxiliar
+	resultado=0
+	for n1 in yar: #para cada elemento n1 del arreglo
+		aux.pop(n1) #Se quita n1 del conjunto auxiliar
+		for n2 in aux: #para cada elemento n2 del conjunto auxiliar
+			if (n1>n2): #comparo n1 con n2
+				break
+		resultado=n1
+		break
+	return resultado
+
+>>> random shuffle
+SyntaxError: invalid syntax
+>>> import random
+>>> p=[]
+>>> for i in range(100):
+	p.append(i)
+
+	
+>>> def minimo(yar):
+	aux=yare #arreglo auxiliar
+	resultado=0
+	for n1 in yar: #para cada elemento n1 del arreglo
+		aux.pop(n1) #Se quita n1 del conjunto auxiliar
+		for n2 in aux: #para cada elemento n2 del conjunto auxiliar
+			if (n1>n2): #comparo n1 con n2
+				break
+		resultado=n1
+		break
+	return resultado
+SyntaxError: multiple statements found while compiling a single statement
+>>> import random
+>>> p=[]
+>>> for i in range(100):
+	p.append(i)
+
+	
+>>> def minimo(yar):
+	aux=yare #arreglo auxiliar
+	resultado=0
+	for n1 in yar: #para cada elemento n1 del arreglo
+		aux.pop(n1) #Se quita n1 del conjunto auxiliar
+		for n2 in aux: #para cada elemento n2 del conjunto auxiliar
+			if (n1>n2): #comparo n1 con n2
+				break
+		resultado=n1
+		break
+	return resultado
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
