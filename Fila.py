727936db7b583fe0e9bcbd3e260fbb8c569f2f25
YarethziGiselle <30918399+YarethziGiselle@users.noreply.github.com>
2017-09-24 23:03:08 -0500

    Add files via upload

---
+++ Fila.py
@@ -0,0 +1,18 @@
+>>> class fila:
+	def __init__(self):
+		self.fila=[]
+	def obtener(self):
+		return self.fila.pop()
+	def meter(self,e):
+		self.fila.append(e)
+		return len(self.fila)
+	@property
+	def longitud(self):
+		return len(self.fila)
+
+	
+>>> l=fila()
+>>> l.meter(2)
+1
+>>> l.meter(5)
+2
